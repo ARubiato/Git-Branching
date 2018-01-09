d1e8737330f984fc2020a32881d906fdda4a1c5e
Alessander Neal L. Rubiato <alessanderrubiato@yahoo.com>
2018-01-10 05:01:03 +0800

    added a list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,11 +9,19 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 	hero: myHero = {
 	id:1, name: 'Leki'
+
+	heroes = HEROES;
+
+
 	};
 	
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: myHero): void {
+    this.selectedHero = hero;
   }
 
 }
